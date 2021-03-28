2f8a62e1e82143fa699c7a5022b4c75d162a54ee
Timur Beletskii <tab282@cornell.edu>
2021-03-27 22:36:14 -0400

    added .ocamlinit

--- _build/board.mli
+++ _build/board.mli
@@ -2,8 +2,14 @@ type t
 
 type color
 
-val make_board : t
+(** [make_board size] initializes a new board with sizes indicated by
+    [size] *)
+val make_board : int * int -> t
 
-val update_board : 'a -> 'b -> unit
+(** [update_board points player board] is [board] with a new connection
+    between [points] owned by [player]*)
+val update_board : 'a -> 'b -> t -> t
 
+(** [get_branch points] is the color of the connection between [points]
+    or blank if no connection exists*)
 val get_branch : 'a -> color
