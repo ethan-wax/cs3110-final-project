a1cafb34247d91dd4d18c9773b72916b5f6b84b5
Evan Wagle <ecw225@cornell.edu>
2021-03-27 21:48:43 -0400

    Added names

--- author.mli
+++ author.mli
@@ -1,5 +1,5 @@
-(** Timur Beletskii (tab282), 
- *)
+(** Timur Beletskii (tab282), Evan Wagle (ecw225), Ethan Waxman (ejw99),
+    Lauren Kiachian (lek93) *)
 
 (** [hours_worked] is the number of hours you worked on this assignment. *)
 val hours_worked : int
