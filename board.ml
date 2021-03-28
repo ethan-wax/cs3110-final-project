f611d440610d7f92885a943db3cf9aaa66d190a0
Ethan Waxman <ejw99@cornell.edu>
2021-03-27 22:25:32 -0400

    Add specifications to board.mli
    - Added specifictaions to board.mli to make it clear what each function
      does
    - Make a change to the number of arguments for update_board in board.ml

--- board.ml
+++ board.ml
@@ -4,6 +4,6 @@ type color = unit
 
 let make_board = failwith "TODO"
 
-let update_board points player = failwith "TODO"
+let update_board points player board = failwith "TODO"
 
-let get_branch point1 point2 = failwith "TODO"
+let get_branch points = failwith "TODO"
