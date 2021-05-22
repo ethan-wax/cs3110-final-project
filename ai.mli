1f361bd52eb2b5587177b8e946aa34d584f021fd
Evan Wagle <ecw225@cornell.edu>
2021-05-22 18:55:29 -0400

    Updated Easy AI mli

--- ai.mli
+++ ai.mli
@@ -1,12 +1,13 @@
 open Board
 
-(* Takes in a board and returns an easy move, in the format "r1 c2 r2
-   c2" *)
+(** Takes in a board and returns a move. Easy AI randomly picks
+    available moves and returns the move in the format "r1 c2 r2 c2". *)
 val easy : Board.t -> string
 
-(* Takes in a board and returns a medium move, in the format "r1 c2 r2
-   c2" *)
+(** Takes in a board and returns a medium move, in the format "r1 c2 r2
+    c2" *)
 val medium : Board.t -> string
 
-(* Takes in a board and returns a hard move, in the format "r1 c2 r2 c2" *)
+(** Takes in a board and returns a hard move, in the format "r1 c2 r2
+    c2" *)
 val hard : Board.t -> string
