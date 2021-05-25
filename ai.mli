db2ad47eb42a99c2c1bca6fd2363cb0b9796e99e
Timur Beletskii <tab282@cornell.edu>
2021-05-24 21:50:33 -0400

    updated ai mli

--- ai.mli
+++ ai.mli
@@ -5,15 +5,19 @@ open Board
     based on the row and col position. *)
 val empty_sides : int -> int -> Board.t -> string list
 
-(** Takes in a board and returns a move. Easy AI randomly picks
-    available moves and returns the move in the format "r1 c2 r2 c2". *)
+(** [easy board] is a move made by an easy level bot. Easy AI randomly
+    picks available moves and returns the move in the format "r1 c2 r2
+    c2". *)
 val easy : Board.t -> string
 
-(** Takes in a board and returns a medium move. Medium AI prioritizes
-    types of moves in this order: Completing boxes, filling in edges,
-    random position. Each move is in the format "r1 c2 r2 c2" *)
+(** [medium board] is a move made by a medium level bot. Medium AI
+    prioritizes types of moves in this order: Completing boxes, filling
+    in edges, random position. Each move is in the format "r1 c2 r2 c2" *)
 val medium : Board.t -> string
 
-(** Takes in a board and returns a hard move, in the format "r1 c2 r2
-    c2" *)
+(** [hard board] is a move made by a hard level bot. Hard AI prioritizes
+    types of moves in this order: Completing boxes, filling in edges,
+    random position. When making these moves, Hard AI tries to avoid
+    moves which will create an opportunity for the other player to score
+    a box. Each move is in the format "r1 c2 r2 c2" *)
 val hard : Board.t -> string
